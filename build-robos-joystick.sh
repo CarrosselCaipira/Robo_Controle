acf12061d547ca30119c25680288d94a2c1d183f
Danilo Weber Nunes <danilownunes@gmail.com>
2018-02-27 23:31:47 -0300

    Correção para compilar no padrão C++11

--- build-robos-joystick.sh
+++ build-robos-joystick.sh
@@ -1,4 +1,4 @@
-g++ -c joystick.cc
-g++ -c robo.cpp
-g++ -c radio.cpp
-g++ -o Robos_Joystick robos_joystick.cpp joystick.o robo.o radio.o 
+g++ -c joystick.cc -std=c++11
+g++ -c robo.cpp -std=c++11
+g++ -c radio.cpp -std=c++11
+g++ -o Robos_Joystick robos_joystick.cpp joystick.o robo.o radio.o -std=c++11
