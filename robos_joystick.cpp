af805498aa5aaaf57bce31eedd807e86995c922d
Danilo Weber Nunes <danilownunes@gmail.com>
2018-10-19 02:06:08 -0300

    Uso da velocidade serial definido em radio.hpp ao invés de setar sua propria velocidade.

--- robos_joystick.cpp
+++ robos_joystick.cpp
@@ -11,7 +11,6 @@ enum MODO_OPERACAO {
   TOTAL_MODO_OPERACAO /* 2 */
 };
 
-static const int INTERVALO_TEMPO = 115200; /**< Intervalo de tempo para a deteccao de eventos do joystick. Deve ser igual ao bit rate da porta serial para nao sobrecarega-la */
 static const int NUM_ROBOS = 3; /**< LEGACY: MANTER EM 3 ENQUANTO O CODIGO DO ARDUINO NAO FOR CORRIGIDO - Deve ser igual ao esperado no código dos arduinos (pois o tamanho do buffer não variável). Ao alterar esta variável, atualizar também o README.md na seção 'Rodando o programa', corrigindo o numero máximo de NUM_JOYSTICKS para ao igual a NUM_ROBOS. */
 static const unsigned char MAX_VELOCIDADE_FRENTE = 7; /**< 0111(7). Vai para frente (bit mais significativo indica sentido da rotacao) com velocidade maxima */
 static const unsigned char MAX_VELOCIDADE_TRAZ = 15; /**< 0111(7) or 1000(8) = 1111(15). Vai para tras (bit mais significativo indica sentido da rotacao) com velocidade maxima */
@@ -27,17 +26,17 @@ struct botoesControle {
   /* Elementos relacionados à JoystickEvent::type = 2 == JS_EVENT_AXIS */
   static const unsigned char AXIS_ESQ_H = 2; /* analogico esquerdo movintos horizontais (eixo X) */
 
-	bool b_L1; /**< se o botao L1 estiver precionado b_L1 = 1, senao b_L1 = 0 */
-	bool b_R1; /**< se o botao R1 estiver precionado b_R1 = 1, senao b_R1 = 0 */
-	bool b_L2; /**< se o botao L2 estiver precionado b_L2 = 1, senao b_L2 = 0 */
-	bool b_R2; /**< se o botao R2 estiver precionado b_R2 = 1, senao b_R2 = 0 */
+  bool b_L1; /**< se o botao L1 estiver precionado b_L1 = 1, senao b_L1 = 0 */
+  bool b_R1; /**< se o botao R1 estiver precionado b_R1 = 1, senao b_R1 = 0 */
+  bool b_L2; /**< se o botao L2 estiver precionado b_L2 = 1, senao b_L2 = 0 */
+  bool b_R2; /**< se o botao R2 estiver precionado b_R2 = 1, senao b_R2 = 0 */
 
   int a_esq_h; /* valores do analogico esquerdo movimento horizontal */
 };
 
 struct Controle{
-	botoesControle botoes_pressionados; /**< indica quais botoes do controle estao pressionados no momento */
-	Joystick *joystick; /**< Ponteiro para instancia de joystick */
+  botoesControle botoes_pressionados; /**< indica quais botoes do controle estao pressionados no momento */
+  Joystick *joystick; /**< Ponteiro para instancia de joystick */
 };
 
 int main(int argc, char const *argv[]) {
@@ -66,63 +65,63 @@ int main(int argc, char const *argv[]) {
   }
 
 
-	std::vector<Controle> controle(NUM_JOYSTICKS); /**< aloca o vetor de structs de joysticks e os botoes que estao precionados no joystick. */
-	for(int i = 0 ; i < NUM_JOYSTICKS; i++)
-		controle[i].joystick = new Joystick(i); /**< instancia do joystic i controlara o robo i. */
+  std::vector<Controle> controle(NUM_JOYSTICKS); /**< aloca o vetor de structs de joysticks e os botoes que estao precionados no joystick. */
+  for(int i = 0 ; i < NUM_JOYSTICKS; i++)
+    controle[i].joystick = new Joystick(i); /**< instancia do joystic i controlara o robo i. */
 
-	std::vector<Robo> robos(NUM_ROBOS); /**< vetor com os NUM_ROBOS robos. */
-	Radio radio(robos); /**< instancia de radio para que possamos enviar os comandos para os robos. */
+  std::vector<Robo> robos(NUM_ROBOS); /**< vetor com os NUM_ROBOS robos. */
+  Radio radio(robos); /**< instancia de radio para que possamos enviar os comandos para os robos. */
 
-	// deteccao dos joysticks
-	for(int i = 0; i < NUM_JOYSTICKS; i++) {
-		// Determinando se eh possivel detectar e utilizar o joystick i
-		if (!controle[i].joystick->isFound()) {
-			printf("Joystick %d nao encontrado. Saindo...\n", i + 1);
-			exit(1);
-		}
-	}
+  // deteccao dos joysticks
+  for(int i = 0; i < NUM_JOYSTICKS; i++) {
+  // Determinando se eh possivel detectar e utilizar o joystick i
+    if (!controle[i].joystick->isFound()) {
+      printf("Joystick %d nao encontrado. Saindo...\n", i + 1);
+      exit(1);
+    }
+  }
 
   /* LOOP Principal */
-	while (true) {
-		// restricao de tempo para fazermos a leitura e envio das velocidades aos robos
-    usleep(INTERVALO_TEMPO);
-		// para cada robo e controle determinar quais os botoes estao precionados e quais nao estao (ou foram liberados agora)
-		for(int i = 0; i < NUM_JOYSTICKS; i++) {
-			JoystickEvent evento_joystick_i;
+  while (true) {
+    // restricao de tempo para fazermos a leitura e envio das velocidades aos robos
+    usleep(Radio::VELOCIDADE_SERIAL);
+    // para cada robo e controle determinar quais os botoes estao precionados e quais nao estao (ou foram liberados agora)
+    for(int i = 0; i < NUM_JOYSTICKS; i++) {
+      JoystickEvent evento_joystick_i;
 
       /* BEGIN DEBUG */
       // fazendo a leitura dos dados dos controles
       // std::cout << evento_joystick_i << '\n';
       /* END DEBUG */
 
-			/* DETECÇÃO DE EVENTOS DISPARADOS PELO JOYSTICK i */
-			if (controle[i].joystick->sample(&evento_joystick_i)) {
-				// Detectando se eh um botao que esta sendo pressionado ou liberado (pode ser um dos analogicos disparando o evento)
-				if (evento_joystick_i.isButton()) {
-					// detectando qual botao esta sendo pressionado ou liberado
-					switch (evento_joystick_i.number) {
-						// L1 esta sendo pressionado
-						case botoesControle::BOTAO_L1:
-							// se for true, significa que o botao esta sendo precionado agora, logo controle[i].botoes_pressionados.b_L1 eh true, do contrario esta sendo liberado.
-							evento_joystick_i.value == true ? controle[i].botoes_pressionados.b_L1 = true : controle[i].botoes_pressionados.b_L1 = false;
-						break;
-						// R1 esta sendo pressionado
-						case botoesControle::BOTAO_R1:
-							// se for true, significa que o botao esta sendo precionado agora, logo controle[i].botoes_pressionados.b_R1 eh true, do contrario esta sendo liberado.
-							evento_joystick_i.value == true ? controle[i].botoes_pressionados.b_R1 = true : controle[i].botoes_pressionados.b_R1 = false;
-						break;
-						// L2 esta sendo pressionado
-						case botoesControle::BOTAO_L2:
-							// se for true, significa que o botao esta sendo precionado agora, logo controle[i].botoes_pressionados.b_L2 eh true, do contrario esta sendo liberado.
-							evento_joystick_i.value == true ? controle[i].botoes_pressionados.b_L2 = true : controle[i].botoes_pressionados.b_L2 = false;
-						break;
-						// R2 esta sendo pressionado
-						case botoesControle::BOTAO_R2:
-							// se for true, significa que o botao esta sendo precionado agora, logo controle[i].botoes_pressionados.b_R2 eh true, do contrario esta sendo liberado.
-							evento_joystick_i.value == true ? controle[i].botoes_pressionados.b_R2 = true : controle[i].botoes_pressionados.b_R2 = false;
-						break;
-					}
-				}
+      /* DETECÇÃO DE EVENTOS DISPARADOS PELO JOYSTICK i */
+      if (controle[i].joystick->sample(&evento_joystick_i)) {
+        // Detectando se eh um botao que esta sendo pressionado ou liberado (pode ser um dos analogicos disparando o evento)
+        if (evento_joystick_i.isButton()) {
+          // detectando qual botao esta sendo pressionado ou liberado
+          switch (evento_joystick_i.number) {
+            // L1 esta sendo pressionado
+            case botoesControle::BOTAO_L1:
+              // se for true, significa que o botao esta sendo precionado agora, logo controle[i].botoes_pressionados.b_L1 eh true, do contrario esta sendo liberado.
+              evento_joystick_i.value == true ? controle[i].botoes_pressionados.b_L1 = true : controle[i].botoes_pressionados.b_L1 = false;
+            break;
+            // R1 esta sendo pressionado
+            case botoesControle::BOTAO_R1:
+              // se for true, significa que o botao esta sendo precionado agora, logo controle[i].botoes_pressionados.b_R1 eh true, do contrario esta sendo liberado.
+              evento_joystick_i.value == true ? controle[i].botoes_pressionados.b_R1 = true : controle[i].botoes_pressionados.b_R1 = false;
+            break;
+            // L2 esta sendo pressionado
+            case botoesControle::BOTAO_L2:
+              // se for true, significa que o botao esta sendo precionado agora, logo controle[i].botoes_pressionados.b_L2 eh true, do contrario esta sendo liberado.
+              evento_joystick_i.value == true ? controle[i].botoes_pressionados.b_L2 = true : controle[i].botoes_pressionados.b_L2 = false;
+            break;
+            // R2 esta sendo pressionado
+            case botoesControle::BOTAO_R2:
+              // se for true, significa que o botao esta sendo precionado agora, logo controle[i].botoes_pressionados.b_R2 eh true, do contrario esta sendo liberado.
+              evento_joystick_i.value == true ? controle[i].botoes_pressionados.b_R2 = true : controle[i].botoes_pressionados.b_R2 = false;
+              break;
+          }
+        }
         else if(evento_joystick_i.isAxis()) {
           switch (evento_joystick_i.number) {
             case botoesControle::AXIS_ESQ_H:
@@ -130,16 +129,31 @@ int main(int argc, char const *argv[]) {
             break;
           }
         }
-			}
-		}
+      }
+    }
 
-		/* SETANDO OS VALORES DE VELOCIDADE DOS ROBOS */
-		for(int i = 0; i < NUM_JOYSTICKS; i++) {
+    /* SETANDO OS VALORES DE VELOCIDADE DOS ROBOS */
+    for(int i = 0; i < NUM_JOYSTICKS; i++) {
       unsigned char rodaEsquerda = 0; /**< indica a velocidade atual da roda esquerda. */
       unsigned char rodaDireita = 0; /**< indica a velocidade atual da roda direita. */
       double modificador;
 
       switch (modoOP) {
+        case CONTROLE_LR:
+          // se L1 esta precionado, a roda esquerda do robo gira para tras
+          if(controle[i].botoes_pressionados.b_L1)
+            rodaEsquerda = MAX_VELOCIDADE_TRAZ;
+          // se L2 esta precionado, a roda esquerda do robo gira para frente
+          if(controle[i].botoes_pressionados.b_L2)
+            rodaEsquerda = MAX_VELOCIDADE_FRENTE;
+          // se R1 esta precionado, a roda direita do robo gira para tras
+          if(controle[i].botoes_pressionados.b_R1)
+            rodaDireita = MAX_VELOCIDADE_TRAZ;
+          // se R2 esta precionado, a roda direita do robo gira para frente
+          if(controle[i].botoes_pressionados.b_R2)
+            rodaDireita = MAX_VELOCIDADE_FRENTE;
+        break;
+
         case CONTROLE_ANALOGICO:
           // se R2 esta precionado, o robo vai para frente
           if(controle[i].botoes_pressionados.b_R2){
@@ -169,31 +183,16 @@ int main(int argc, char const *argv[]) {
             rodaEsquerda = rodaEsquerda * (1 - modificador);
           }
         break;
-
-        case CONTROLE_LR:
-          // se L1 esta precionado, a roda esquerda do robo gira para tras
-          if(controle[i].botoes_pressionados.b_L1)
-            rodaEsquerda = MAX_VELOCIDADE_TRAZ;
-          // se L2 esta precionado, a roda esquerda do robo gira para frente
-          if(controle[i].botoes_pressionados.b_L2)
-            rodaEsquerda = MAX_VELOCIDADE_FRENTE;
-          // se R1 esta precionado, a roda direita do robo gira para tras
-          if(controle[i].botoes_pressionados.b_R1)
-            rodaDireita = MAX_VELOCIDADE_TRAZ;
-          // se R2 esta precionado, a roda direita do robo gira para frente
-          if(controle[i].botoes_pressionados.b_R2)
-            rodaDireita = MAX_VELOCIDADE_FRENTE;
-        break;
       }
 
       /* BEGIN DEBUG */
       // std::cout << (int)rodaEsquerda<< " " << (int)rodaDireita<< '\n';
       /* END DEBUG */
 
-			// gravando valores de velocidade nos robos
-			robos[i].setVelocidadeAtualRobo(rodaEsquerda, rodaDireita);
-		}
-		// faz o envio das velocidades para todos os robos via radio
-		radio.enviaDados();
-	}
+      // gravando valores de velocidade nos robos
+      robos[i].setVelocidadeAtualRobo(rodaEsquerda, rodaDireita);
+    }
+    // faz o envio das velocidades para todos os robos via radio
+    radio.enviaDados();
+  }
 }
