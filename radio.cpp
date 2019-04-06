6d00c66704861ebfbc5541abeee5f43e61bea80f
Danilo Weber Nunes <danilownunes@gmail.com>
2019-04-06 17:39:33 -0300

    Comunicação agora funciona de forma temporizada.

--- radio.cpp
+++ radio.cpp
@@ -3,94 +3,143 @@
 /* o trecho: vector_robos(v) serve para acoplar o vector com os robos a classe de transmissao */
 Radio::Radio(std::vector<Robo>& v) : vector_robos(v) {
 
-  /* abrindo a porta serial para leitura e escrita */
-  this->USB = open(this->caminho_dispositivo, O_RDWR| O_NOCTTY | O_NDELAY);
+    /* abrindo a porta serial para leitura e escrita */
+    this->USB = open(this->caminho_dispositivo, O_RDWR| O_NOCTTY | O_NDELAY);
 
-  /* setando todos os campos de dispositivo_tty com 0 (vamos evitar surpresas...) */
-  memset(&this->dispositivo_tty, 0, sizeof(this->dispositivo_tty));
+    /* setando todos os campos de dispositivo_tty com 0 (vamos evitar surpresas...) */
+    memset(&this->dispositivo_tty, 0, sizeof(this->dispositivo_tty));
 
-  /* Tratamento de Erros */
-  /* Testado se foi possivel abrir a porta serial */
-  if(this->USB < 0) {
-    std::cerr << "Erro " << errno << " @Radio->open " << this->caminho_dispositivo << ": " << std::strerror(errno) << std::endl;
-    exit(1); /**< Nao permite que o programa rode se nao foi possivel configurar a porta serial */
-  }
+    /* Tratamento de Erros */
+    /* Testado se foi possivel abrir a porta serial */
+    if(this->USB < 0) {
+        std::cerr << "Erro " << errno << " @Radio->open " << this->caminho_dispositivo << ": " << std::strerror(errno) << std::endl;
+        exit(1); /**< Nao permite que o programa rode se nao foi possivel configurar a porta serial */
+    }
 
-  /* Testando se a porta serial aberta esta apontando para um dispositivo TTY (nosso radio eh TTY) */
-  if(isatty(this->USB) < 0) {
-    std::cerr << "Erro " << errno << " @Radio->isatty: " << std::strerror(errno) << std::endl;
-    exit(1); /**< Nao permite que o programa rode se nao foi possivel configurar a porta serial */
-  }
-  /* Testando se a atual configuracao da porta serial pode ser lida */
-  if(tcgetattr( this->USB, &this->dispositivo_tty ) < 0 ) {
-    std::cerr << "Erro " << errno << " @Radio->tcgetattr: " << std::strerror(errno) << std::endl;
-    exit(1); /**< Nao permite que o programa rode se nao foi possivel configurar a porta serial */
-  }
-  /* Setando a frequencia de input e output do radio */
-  if(cfsetspeed(&this->dispositivo_tty, Radio::VELOCIDADE_SERIAL) < 0){
-    std::cerr << "Erro " << errno << " @Radio->cfsetospeed: " << std::strerror(errno) << std::endl;
-    exit(1); /**< Nao permite que o programa rode se nao foi possivel configurar a porta serial */
-  }
+    /* Testando se a porta serial aberta esta apontando para um dispositivo TTY (nosso radio eh TTY) */
+    if(isatty(this->USB) < 0) {
+        std::cerr << "Erro " << errno << " @Radio->isatty: " << std::strerror(errno) << std::endl;
+        exit(1); /**< Nao permite que o programa rode se nao foi possivel configurar a porta serial */
+    }
+    /* Testando se a atual configuracao da porta serial pode ser lida */
+    if(tcgetattr( this->USB, &this->dispositivo_tty ) < 0 ) {
+        std::cerr << "Erro " << errno << " @Radio->tcgetattr: " << std::strerror(errno) << std::endl;
+        exit(1); /**< Nao permite que o programa rode se nao foi possivel configurar a porta serial */
+    }
+    /* Setando a frequencia de input e output do radio */
+    if(cfsetspeed(&this->dispositivo_tty, Radio::VELOCIDADE_SERIAL) < 0){
+        std::cerr << "Erro " << errno << " @Radio->cfsetospeed: " << std::strerror(errno) << std::endl;
+        exit(1); /**< Nao permite que o programa rode se nao foi possivel configurar a porta serial */
+    }
 
-  /* Setando outras configuracoes da porta (no momento: aceite que funciona)*/
-  this->dispositivo_tty.c_iflag = IGNBRK | IGNPAR;
-  this->dispositivo_tty.c_oflag = 0;
-  this->dispositivo_tty.c_lflag = 0;
-  this->dispositivo_tty.c_cflag = Radio::VELOCIDADE_SERIAL | CS8;
+    /* Setando outras configuracoes da porta (no momento: aceite que funciona)*/
+    this->dispositivo_tty.c_iflag = IGNBRK | IGNPAR;
+    this->dispositivo_tty.c_oflag = 0;
+    this->dispositivo_tty.c_lflag = 0;
+    this->dispositivo_tty.c_cflag = Radio::VELOCIDADE_SERIAL | CS8;
 
-  /* Setagem para realizar a 'leitura contada', a leitura da porta serial eh efetuada apenas se o buffer de recebimento possui VMIN bytes(chars). Detalhes mais aprofundados podem ser encontrados em: VSS/Docs/Comunicacao_Serial/Understanding UNIX termios VMIN and VTIME.pdf na ultima pagina no topico VMIN > 0 and VTIME = 0 */
-  this->dispositivo_tty.c_cc[VMIN] = 1; /**< aguarda que seja recebido 1 byte (char) para efetuar a leitura. Senao tiver chego, bloqueia o processo. */
-  this->dispositivo_tty.c_cc[VTIME] = 0; /**< nao ha timing para fazer a leitura da porta */
+    /* Setagem para realizar a 'leitura contada', a leitura da porta serial eh efetuada apenas se o buffer de recebimento possui VMIN bytes(chars). Detalhes mais aprofundados podem ser encontrados em: VSS/Docs/Comunicacao_Serial/Understanding UNIX termios VMIN and VTIME.pdf na ultima pagina no topico VMIN > 0 and VTIME = 0 */
+    this->dispositivo_tty.c_cc[VMIN] = 1; /**< aguarda que seja recebido 1 byte (char) para efetuar a leitura. Senao tiver chego, bloqueia o processo. */
+    this->dispositivo_tty.c_cc[VTIME] = 0; /**< nao ha timing para fazer a leitura da porta */
 
-  /* descarta dos dados atualmente na porta ('lixo' de memoria)*/
-  tcflush(this->USB, TCIOFLUSH);
-  /* Aplicando as configuracoes.
-   * TCSANOW = aplica instantaneamente as configuracoes.
-   */
-  if (tcsetattr(this->USB, TCSANOW, &this->dispositivo_tty ) < 0) {
-    std::cerr << "Error " << errno << " @Radio->tcsetattr " << std::strerror(errno) << std::endl;
-    exit(1); /**< Nao permite que o programa rode se nao foi possivel configurar a porta serial */
-  }
+    /* descarta dos dados atualmente na porta ('lixo' de memoria)*/
+    tcflush(this->USB, TCIOFLUSH);
+    /* Aplicando as configuracoes.
+    * TCSANOW = aplica instantaneamente as configuracoes.
+    */
+    if (tcsetattr(this->USB, TCSANOW, &this->dispositivo_tty ) < 0) {
+        std::cerr << "Error " << errno << " @Radio->tcsetattr " << std::strerror(errno) << std::endl;
+        exit(1); /**< Nao permite que o programa rode se nao foi possivel configurar a porta serial */
+    }
+    this->num_bytes_enviados = 2 * this->vector_robos.size() + 1; /**< Dois bytes para cada robo (um para cada roda) mais um, inicial, que indica o inicio de uma nova sequência. */
+
+    Radio::calculaTempoSleep();
+}
+
+Radio::~Radio() {
+    /* indicando que a thread deve parar sua execução */
+    this->comunicacao_terminada = true;
+
+    /* checando se é possivel unir a thread de comunicacao à principal. */
+    if (this->thread_envio->joinable())
+        Radio::terminaComunicacao();
+
+    close(this->USB);
 }
 
 /* enviando um byte para cada roda. Futuramente armazenaremos em um byte os valores de velocidade de ambas as rodas >> necessario corrigir robo.hpp robo.cpp tipoEstruturas.hpp e os codigos do arduino. */
 void Radio::enviaDados() {
-  /* aparentemente desnecessario, descomentar em caso de problemas */
-  /* descarta dos dados atualmente na porta ('lixo' de memoria) */
-  tcflush( this->USB, TCIOFLUSH);
+    /* aparentemente desnecessario, descomentar em caso de problemas */
+    /* descarta dos dados atualmente na porta ('lixo' de memoria) */
+    tcflush(this->USB, TCIOFLUSH);
 
-  /* o primeiro (dados[0]) eh o valor de velocidade do motor da roda esquerda do primeiro robo */
-  /* o segundo (dados[1]) eh o valor de velocidade do motor da roda direita do primeiro robo */
-  /* o terceiro (dados[2]) eh o valor de velocidade do motor da roda esquerda do segundo robo */
-  /* o quarto (dados[3]) eh o valor de velocidade do motor da roda direita do segundo robo */
-  /* e assim por diante para todos os robos do time em campo */
-  /* os ultimos dois elementos do vetor(bytes) correspondem ao caractere zero */
-  int numBytesEnviados = 2 * this->vector_robos.size() + 2; /**< Dois bytes para cada robo (um para cada roda) mais dois caracteres zeros, que vao no final (um byte para cada) */
-  unsigned char dados[numBytesEnviados];
+    /* o primeiro (dados[0]) eh o caractere inicial para delimitar o inicio de uma nova sequencia de comandos */
+    /* o segundo (dados[1]) eh o valor de velocidade do motor da roda esquerda do primeiro robo */
+    /* o terceiro (dados[2]) eh o valor de velocidade do motor da roda direita do primeiro robo */
+    /* o quarto (dados[3]) eh o valor de velocidade do motor da roda esquerda do segundo robo */
+    /* o quinto (dados[4]) eh o valor de velocidade do motor da roda direita do segundo robo */
+    /* e assim por diante para todos os robos do time em campo */
+    /* os ultimos dois elementos do vetor(bytes) correspondem ao caractere zero */
 
-  /* envia 0x80 como primeiro sinal (sera interpretado pelos robos) */
-  unsigned char caractere_inicial = 0x80;
-  if(write(this->USB, &caractere_inicial, 1) < 0)
-    std::cerr << "Error " << errno << " @enviaDados->write_1 " << std::strerror(errno) << std::endl;
+    unsigned char dados[this->num_bytes_enviados];
+    dados[0] = this->caractere_inicial;
+    /* preenchendo o vetor de dados das rodas com os valores de velocidade dos robos */
+    for(int i = 0; i < this->vector_robos.size(); i++) {
+        /* deslocamos para esquerda os ultimos 4 bits para que eles sejam os primeiros do byte. Para os robos que receberao, apenas os primeiros 4 bytes sao importantes para os robos. */
+        dados[2 * i + 1] = this->vector_robos[i].getVelocidadeAtualRobo().rodaEsq;
+        dados[2 * i + 2] = this->vector_robos[i].getVelocidadeAtualRobo().rodaDir;
+    }
 
-  /* preenchendo o vetor de dados das rodas com os valores de velocidade dos robos */
-  for(int i = 0; i < this->vector_robos.size(); i++) {
-    /* deslocamos para esquerda os ultimos 4 bits para que eles sejam os primeiros do byte. Para os robos que receberao, apenas os primeiros 4 bytes sao importantes para os robos. */
-    dados[2 * i] = this->vector_robos[i].getVelocidadeAtualRobo().rodaEsq;
-    dados[2 * i + 1] = this->vector_robos[i].getVelocidadeAtualRobo().rodaDir;
-
-    // dados[2 * i] = 4;
-    // dados[2 * i + 1] = 4;
-}
-  /* os dois ultimos bytes enviados sao zero. No momento, aceite... */
-  dados[2 * this->vector_robos.size()] = '0';
-  dados[2 * this->vector_robos.size() + 1] = '0';
-
-  /* faz o envio das velocidades de cada roda para a porta serial */
-  if(write(this->USB, dados, numBytesEnviados) < 0)
-    std::cerr << "Error " << errno << " @enviaDados->write_2 " << std::strerror(errno) << std::endl;
+    /* faz o envio das velocidades de cada roda para a porta serial */
+    if(write(this->USB, dados, this->num_bytes_enviados) < 0)
+        std::cerr << "Error " << errno << " @Radio::enviaDados->write " << std::strerror(errno) << std::endl;
 }
 
-Radio::~Radio() {
-  close(this->USB);
+void Radio::comecaComunicacao() {
+    /* se é a primeira chamada desta função, prepara os parametros para a execução da thread. */
+    if(!this->comunicacao_pronta) {
+        this->comunicacao_pausada = false; /* indicando que a thread agora esta ativa. */
+        this->comunicacao_pronta = true; /* indicando que já ajustamos os parametros que precisavamos. (não vamos mais executar esse trecho de novo) */
+        this->thread_envio = new std::thread(&Radio::comecaComunicacao, this); /* instanciando a thread. */
+
+        return; /* saimos pois já fizemos a configuração, a thread agora está rodando até que solicitemos sua parada. */
+    }
+
+    /* enquanto a comunicacao não é terminanda, faz o envio dos dados para a serial*/
+    while(!this->comunicacao_terminada) {
+        /* se a comunicação não está em pausa, pode fazer o envio dos dados, senão apenas espera. */
+        if(!this->comunicacao_pausada)
+            Radio::enviaDados();
+
+        /* espera para executar novamente e manter a sincronia com o rádio */
+        std::this_thread::sleep_for(std::chrono::microseconds(this->THREAD_SLEEP_TIME));
+    }
 }
+
+void Radio::terminaComunicacao() {
+    /* indicando que a thread deve terminar sua execução */
+    this->comunicacao_terminada = true;
+
+    this->thread_envio->join();
+}
+
+void Radio::pausaComunicacao() {
+    /* indicando que a thread deve parar de enviar os dados durante sua execução */
+    this->comunicacao_pausada = true;
+}
+
+void Radio::recomecaComunicacao() {
+    /* indicando que a thread deve voltar a enviar os dados durante sua execução  */
+    this->comunicacao_pausada = false;
+
+}
+
+void Radio::calculaTempoSleep() {
+    /* calculando o numero total de bits que irão ser enviados pela porta serial */
+    double total_bits = this->num_bytes_enviados * this->BITS_PER_BYTE;
+
+    /* calculando quanto tempo (em microsegundos) vamos precisar para enviar o numero total de bits (total_bits) */
+    this->THREAD_SLEEP_TIME = int((total_bits / this->VELOCIDADE_SERIAL_INT) * 1000000);
+}
+
+
--- radio.hpp
+++ radio.hpp
@@ -19,6 +19,9 @@
 #include <termios.h> // Definicoes de controle de terminal POSIX
 #include <vector>
 #include <iostream>
+#include <thread>
+#include <chrono>
+
 #include "robo.hpp"
 
 
@@ -32,15 +35,39 @@
  *  @todo Futuramente tambem implementaremos o recebimento.
  */
 class Radio {
+  const unsigned char caractere_inicial = 0x80; /* envia 0x80 como primeiro sinal (sera interpretado pelos robos) */	
+  int num_bytes_enviados = 0; /**< Numeoro de bytes a serem enviados pela serial. Dois bytes para cada robo (um para cada roda) mais um, inicial, que indica o inicio de uma nova sequência. */
   std::vector<Robo>& vector_robos; /**< referencia para o vector que contem os robos  */
   struct termios dispositivo_tty; /**< estrutura de configuracao da porta serial */
   int USB; /**< descritor da porta serial a ser lida */
   const char* caminho_dispositivo = "/dev/ttyUSB0"; /**< caminho para a porta a ser aberta para comunicao serial */
 
-  public:
-    static const speed_t VELOCIDADE_SERIAL = B115200; /* frequencia de input e output da porta serial. Bit rate padrao de 115200. Disponíveis: 115200, 9600. Mantida publica para ser usada por outras classes que fazem uso do radio. */
+  bool comunicacao_terminada = false; /**< indica se a comunicação foi terminada ou não (thread deve continuar existindo (false) deve parar (true). */
+  bool comunicacao_pausada = true; /**< indica se a comunicação está em pausa ou não (thread de envio esta enviando dados (false) ou esta em pausa (existe mais não esta enviando nada) (true). */
+  /* flag que indica se é a primera chamada da função startComunicacao(). Na primeira chamada ela prepara a infraestrutura para
+   * a execução da thread de comunicação e executa a ação em si, a partir daí apenas execurá a ação. */
+  bool comunicacao_pronta = false;
+  std::thread *thread_envio = nullptr; /**< thread temporizada que fará o envio dos dados à porta serial executando a  funcao Radio->enviaDados() */
 
-   /** @fn Radio(std::vector<Robo>& v)
+  public:
+    /* frequencia de input e output da porta serial. Bit rate padrao de 115200. Disponíveis: 115200, 9600. Mantida
+     * publica para ser usada por outras classes que fazem uso do radio e também na variável Radio::VELOCIDADE_SERIAL_INT*/
+    static const speed_t VELOCIDADE_SERIAL = B115200;
+
+    /* bit rate convertido para inteiro (usado na temporização da thread). Deve ser igual a Radio::VELOCIDADE_SERIAL */
+    const unsigned int VELOCIDADE_SERIAL_INT = 115200;
+
+    /* número de bits que são enviados à porta serial para concluir o envio de 1 byte (8 bits).
+     * Isso ocorre pois existem mais bits sendo enviados além dos que estamos enviando. Logo, caso queiramos mandar 8 bits,
+     * e o canal que possui 1 bit de start, 1 bit de stop e não possui bit de paridade teremos:
+     * 1(start) + 1(stop) + 0(paridade) + 8(nossos dados) = 10 bits. Este valor pode mudar de canal para canal, logo precisa ser
+     * pesquisado antes de ser alterado. (não trocar e testar para ver o que acontece Sherlock).
+     * Essa variável é usada para calcular o tempo que a thread de startComuncacao. */
+    const unsigned int BITS_PER_BYTE = 10;
+
+    unsigned int THREAD_SLEEP_TIME; /* tempo que a thread dormirá esperando a proxima execução (em microsegundos) */
+
+    /** @fn Radio(std::vector<Robo>& v)
     *  @brief Faz a configuracao da porta serial para efetuar a comunicao e a 'acoplagem' do vetor de robos.
     *  @param std::vector<Robo>& v Vetor contendo os robos do time.
     */
@@ -55,6 +82,22 @@ class Radio {
      *  @brief Faz o envio dos valores de velocidade de estadoAtualRobo de cada robo em campo
      */
     void enviaDados();
+
+    /**
+     *
+     */
+    void comecaComunicacao();
+
+    void terminaComunicacao();
+
+    void pausaComunicacao();
+
+    void recomecaComunicacao();
+
+    /** @fn calculaTempoSleep()
+     *  @brief Calcula o tempo que thread de envio de dados precisá dormir para manter a sincronia com o rádio
+     */
+    void calculaTempoSleep();
 
 };
 
